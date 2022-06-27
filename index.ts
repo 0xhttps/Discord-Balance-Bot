0439c3044b0649fb83db674a2649ecce3e4502db
0xhttps <mjquinn11@gmail.com>
2022-06-26 18:00:10 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -17,76 +17,89 @@ client.on('ready', () => {
 })
 
 client.on('messageCreate', async (message) => {
-    if (message.content.endsWith(".eth")) {
-        var ENS = message.content
-        var wallet = await web3.eth.ens.getAddress(ENS)
-        const walletBalance = await web3.eth.getBalance(wallet)
-        const displayWallet = ENS
-        if(walletBalance.length === 20) {
-            const walletBalanceFinal = walletBalance.slice(0,2)+ "." + walletBalance.slice(3,5) + " ETH"
-            message.reply({ 
-            content: "Balance of " + displayWallet + ": " + walletBalanceFinal
-            })
-        } 
-        if(walletBalance.length === 19) {
-            const walletBalanceFinal = walletBalance.charAt(0)+ "." + walletBalance.slice(1,4) + " ETH"
-            message.reply({ 
-            content: "Balance of " + displayWallet + ": " + walletBalanceFinal
-            })
-        }
-        if(walletBalance.length === 18) {
-            const walletBalanceFinal = "0." + walletBalance.slice(0,3) + " ETH"
-            message.reply({ 
-            content: "Balance of " + displayWallet + ": " + walletBalanceFinal
-            })
-        }
-        if(walletBalance.length === 17) {
-            const walletBalanceFinal = "0.0" + walletBalance.slice(0,3) + " ETH"
-            message.reply({ 
-            content: "Balance of " + displayWallet + ": " + walletBalanceFinal
-            })
-        }
-        if(walletBalance.length === 21) {
-            const walletBalanceFinal = walletBalance.slice(0,3)+ "." + walletBalance.slice(4,6) + " ETH"
-            message.reply({ 
-            content: "Balance of " + displayWallet + ": " + walletBalanceFinal
-            })
-        }
-    }
-    if (message.content.startsWith("0x") && message.content.length === 42) {
+
+    const returnAddress = async () => {
         var wallet = message.content
         const displayWallet = "0x***" + wallet.slice(38,42)
-        const walletBalance = await web3.eth.getBalance(wallet)
-        if(walletBalance.length === 20) {
-            const walletBalanceFinal = walletBalance.slice(0,2)+ "." + walletBalance.slice(3,5) + " ETH"
-            message.reply({ 
-            content: "Balance of " + displayWallet + ": " + walletBalanceFinal
+        return displayWallet
+    }
+
+    const getBalanceENS = async () => {
+        try {
+            var ENS = message.content
+            const wallet = await web3.eth.ens.getAddress(ENS)
+            const walletBalance = await web3.eth.getBalance(wallet)
+            if(walletBalance.length === 20) {
+                const walletBalanceFinal = walletBalance.slice(0,2)+ "." + walletBalance.slice(3,5) + " ETH"
+                return walletBalanceFinal
+            } 
+            if(walletBalance.length === 19) {
+                const walletBalanceFinal = walletBalance.charAt(0)+ "." + walletBalance.slice(1,4) + " ETH"
+                return walletBalanceFinal
+            }
+            if(walletBalance.length === 18) {
+                const walletBalanceFinal = "0." + walletBalance.slice(0,3) + " ETH"
+                return walletBalanceFinal
+            }
+            if(walletBalance.length === 17) {
+                const walletBalanceFinal = "0.0" + walletBalance.slice(0,3) + " ETH"
+                return walletBalanceFinal
+            }
+            if(walletBalance.length === 21) {
+                const walletBalanceFinal = walletBalance.slice(0,3)+ "." + walletBalance.slice(4,6) + " ETH"
+                return walletBalanceFinal
+            }
+        }  catch (err){
+            console.log(err)
+        }
+    }
+
+    const getBalance0x = async () => {
+        try {
+            var wallet = message.content
+            const walletBalance = await web3.eth.getBalance(wallet)
+            if(walletBalance.length === 20) {
+                const walletBalanceFinal = walletBalance.slice(0,2)+ "." + walletBalance.slice(3,5) + " ETH"
+                return walletBalanceFinal
+            } 
+            if(walletBalance.length === 19) {
+                const walletBalanceFinal = walletBalance.charAt(0)+ "." + walletBalance.slice(1,4) + " ETH"
+                return walletBalanceFinal
+            }
+            if(walletBalance.length === 18) {
+                const walletBalanceFinal = "0." + walletBalance.slice(0,3) + " ETH"
+                return walletBalanceFinal
+            }
+            if(walletBalance.length === 17) {
+                const walletBalanceFinal = "0.0" + walletBalance.slice(0,3) + " ETH"
+                return walletBalanceFinal
+            }
+            if(walletBalance.length === 21) {
+                const walletBalanceFinal = walletBalance.slice(0,3)+ "." + walletBalance.slice(4,6) + " ETH"
+                return walletBalanceFinal
+            }
+        }  catch (err){
+            console.log(err)
+        }
+    }
+
+    try {
+        if(message.content.endsWith(".eth")) {
+            const balance = await getBalanceENS()
+            message.reply({
+                content: "Wallet balance of " + message.content + ": " + balance
+            })
+        } else if(message.content.startsWith("0x") && message.content.length === 42) {
+            const balance = await getBalance0x()
+            const displayWallet = await returnAddress()
+            message.reply({
+                content: "Wallet balance of " + displayWallet + ": " + balance
             })
         }
-        if(walletBalance.length === 19) {
-            const walletBalanceFinal = walletBalance.charAt(0)+ "." + walletBalance.slice(1,4) + " ETH"
-            message.reply({ 
-            content: "Balance of " + displayWallet + ": " + walletBalanceFinal
-            })
-        }
-        if(walletBalance.length === 18) {
-            const walletBalanceFinal = "0." + walletBalance.slice(0,3) + " ETH"
-            message.reply({ 
-            content: "Balance of " + displayWallet + ": " + walletBalanceFinal
-            })
-        }
-        if(walletBalance.length === 17) {
-            const walletBalanceFinal = "0.0" + walletBalance.slice(0,3) + " ETH"
-            message.reply({ 
-            content: "Balance of " + displayWallet + ": " + walletBalanceFinal
-            })
-        }
-        if(walletBalance.length === 21) {
-            const walletBalanceFinal = walletBalance.slice(0,3)+ "." + walletBalance.slice(4,6) + " ETH"
-            message.reply({ 
-            content: "Balance of " + displayWallet + ": " + walletBalanceFinal
-            })
-        }
+    } catch (err) {
+        message.reply({
+            content: "Please enter a valid address"
+        })
     }
 })
 client.login(process.env.TOKEN)
